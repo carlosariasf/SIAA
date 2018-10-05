fff369409c4ad16fa4591e0eadc61c8b406ff378
Erika <Erika@Erika-PC.mshome.net>
2018-10-05 13:18:46 -0500

    VentanaInicial

--- src/edatos/siaa/com/VentanaInicial.java
+++ src/edatos/siaa/com/VentanaInicial.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaInicial extends JFrame {
 
@@ -14,6 +19,7 @@ public class VentanaInicial extends JFrame {
 	 */
 	private static final long serialVersionUID = -3401325008279193550L;
 	private JPanel contentPane;
+	private JTextField textNGrup;
 
 	/**
 	 * Launch the application.
@@ -39,8 +45,39 @@ public class VentanaInicial extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblBienvenidParaContinuar = new JLabel("Bienvenid@ para continuar cree un nuevo grupo");
+		lblBienvenidParaContinuar.setBounds(87, 54, 278, 14);
+		contentPane.add(lblBienvenidParaContinuar);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(47, 94, 57, 14);
+		contentPane.add(lblNombre);
+		
+		textNGrup = new JTextField();
+		textNGrup.setBounds(116, 91, 217, 20);
+		contentPane.add(textNGrup);
+		textNGrup.setColumns(10);
+		final VentanaNuevoIntegrante estudiante=new VentanaNuevoIntegrante();
+		
+		JButton btnCrearNuevoGrupo = new JButton("Crear Nuevo Grupo");
+		btnCrearNuevoGrupo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				estudiante.setVisible(true);
+			}
+		});
+		btnCrearNuevoGrupo.setBounds(70, 152, 148, 23);
+		contentPane.add(btnCrearNuevoGrupo);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		btnCancelar.setBounds(268, 152, 89, 23);
+		contentPane.add(btnCancelar);
 	}
-
 }
